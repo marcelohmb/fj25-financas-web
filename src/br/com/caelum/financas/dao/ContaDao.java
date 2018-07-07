7f37ef2935e5d8c94f316be91516e4b8825b66aa
marcelohmb <marcelo.hmb@gmail.com>
2018-07-06 22:44:12 -0300

    QuintaAula

--- src/br/com/caelum/financas/dao/ContaDao.java
+++ src/br/com/caelum/financas/dao/ContaDao.java
@@ -2,20 +2,48 @@ package br.com.caelum.financas.dao;
 
 import java.util.List;
 
+import javax.annotation.Resource;
+import javax.ejb.EJBException;
 import javax.ejb.Stateless;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
 
 import br.com.caelum.financas.modelo.Conta;
 
+@TransactionManagement(TransactionManagementType.BEAN)
 @Stateless
 public class ContaDao {
 
+	@Resource
+	private UserTransaction ut;
+	
 	@PersistenceContext
 	EntityManager manager;
+	
 
 	public void adiciona(Conta conta) {
+		
+		try {
+			this.ut.begin();
+		} catch (Exception e) {
+			throw new EJBException(e);
+		}
+		
 		this.manager.persist(conta);
+		
+		try {
+			this.ut.commit();
+		} catch (Exception e) {
+			try {
+				this.ut.rollback();
+			} catch (Exception e1) {
+				throw new EJBException(e1);
+			}
+			throw new EJBException(e);
+		}
 	}
 
 	public Conta busca(Integer id) {
--- src/br/com/caelum/financas/service/Agendador.java
+++ src/br/com/caelum/financas/service/Agendador.java
@@ -4,16 +4,53 @@ import java.util.concurrent.TimeUnit;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
 import javax.ejb.AccessTimeout;
+import javax.ejb.EJB;
+import javax.ejb.ScheduleExpression;
 import javax.ejb.Singleton;
+import javax.ejb.Timeout;
+import javax.ejb.Timer;
+import javax.ejb.TimerConfig;
+import javax.ejb.TimerService;
+
+import br.com.caelum.financas.dao.ContaDao;
 
 //@Stateless
 @Singleton
 @AccessTimeout(unit = TimeUnit.SECONDS, value=5)
 public class Agendador {
 
+	@EJB
+	private ContaDao contaDao;
+	
 	private static int totalCriado;
+	
+	@Resource
+	private TimerService timerService;
+	
+	public void agenda(String expressaoMinutos, String expressaoSegundos){
+		ScheduleExpression expression = new ScheduleExpression();
+		expression.hour("*");
+		expression.minute(expressaoMinutos);
+		expression.second(expressaoSegundos);
+		
+		TimerConfig config = new TimerConfig();
+		config.setInfo(expression.toString());
+		config.setPersistent(false);
+		
+		this.timerService.createCalendarTimer(expression, config);
+		
+		System.out.println("Agendamento: " + expression);
+	}
 
+	@Timeout
+	public void verificacaoPeriodicaSeHaNovasContas(Timer timer){
+		System.out.println(timer.getInfo());
+		System.out.println(contaDao.lista()); 
+	} 
+	
+	
 	public void executa() {
 		System.out.printf("%d instancias criadas %n", totalCriado);
 
